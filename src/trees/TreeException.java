6c445dc84b1fe04a1d18dd5e2d23fac6b8b7a1f2
Tanya Reuben <tanyareuben@gmail.com>
2022-04-29 15:51:39 -0700

    initial checkin

---
+++ src/trees/TreeException.java
@@ -0,0 +1,12 @@
+package trees;
+
+import javax.swing.tree.DefaultMutableTreeNode;
+
+public class TreeException extends DefaultMutableTreeNode
+{
+	public TreeException(String s)
+	{
+		// Pass s into a superclass ctor.
+		super(s); 
+	}
+}
